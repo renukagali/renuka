41790057afe0d5f7bfa0008f718c9f42a3649273
Chay-k <Chaitanya.Kakarla-ext@ab-Inbev.com>
2024-03-28 15:30:35 +0530

    admin add

--- my_venv/Scripts/customuser/customuserapp/admin.py
+++ my_venv/Scripts/customuser/customuserapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import NewUser
+from customuserapp.models import NewUser
 
 
 admin.site.register(NewUser)
--- my_venv/Scripts/customuser/dealerapp/admin.py
+++ my_venv/Scripts/customuser/dealerapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Dealer
+from dealerapp.models import Dealer
 
 
 admin.site.register(Dealer)
